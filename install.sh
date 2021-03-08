ec7ffb65c203a5e4afcba8a7de097c1e146447cc
Théophile Gaudin <theophile.gaudin@gmail.com>
2021-03-08 10:30:15 +0100

    zinit and zshrc added to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+###
+### zsh
+###
+
+# zinit install
+if [ ! -d ${HOME}/.zinit ]; then
+  mkdir ${HOME}/.zinit
+  git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
+fi
+
+ln -s $(pwd)/zshrc $HOME/.zshrc
+
+###
+### neovim config install
+###
+
+# Plug
+if [ ! -d ${XDG_DATA_HOME:-$HOME/.local/share} ]; then 
+  curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Config file
 mkdir -p $HOME/.config/nvim/
 ln -s $(pwd)/init.vim $HOME/.config/nvim/init.vim
 
